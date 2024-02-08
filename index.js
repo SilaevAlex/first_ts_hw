6fbc2fc81f56c89d3fba833059d9312755c11d76
Aleksandr <dp110393sad@gmail.com>
2024-02-08 18:17:26 +0200

    add code to index and compilation

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 // let name :string = "John";
 // console.log("Имя:", name);
-
 // let age = 30;
 // console.log("Возраст:", age);
-
 // let city = "New York";
 // console.log("Город:", city);
-
-const greet = (name :string) => {
+var greet = function (name) {
     console.log("Привет,", name + "!");
 };
-
-const sum = (a :number, b :number) => {
-    let result = a + b;
-    console.log( a, "+", b, "=", result);
+var sum = function (a, b) {
+    var result = a + b;
+    console.log(a, "+", b, "=", result);
     return result;
 };
-
 greet("Alice");
-let total = sum(5, 3);
+var total = sum(5, 3);
